f85f9a6668941356225ff1f7552c0af22adc0e46
缥缈xy <1282127821@qq.com>
2019-11-16 09:10:26 +0800

    2019年11月16日09:10:20

---
+++ src/main/java/com/xy/状态机/BookStates.java
@@ -0,0 +1,9 @@
+package com.xy.状态机;
+
+
+public enum BookStates {
+    AVAILABLE,
+    BORROWED,
+    IN_REPAIR
+}
+
---
+++ src/main/java/com/xy/状态机/StatemachineApplication.java
@@ -0,0 +1,32 @@
+package com.xy.状态机;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.statemachine.StateMachine;
+import org.springframework.statemachine.config.EnableStateMachine;
+
+@SpringBootApplication
+@EnableStateMachine
+public class StatemachineApplication implements CommandLineRunner {
+    private final StateMachine<BookStates, BookEvents> stateMachine;
+
+    public static void main(String[] args) {
+        SpringApplication.run(StatemachineApplication.class, args);
+    }
+
+    @Autowired
+    public StatemachineApplication(StateMachine<BookStates, BookEvents> stateMachine) {
+        this.stateMachine = stateMachine;
+    }
+
+    @Override
+    public void run(String... args) {
+//        stateMachine.start();
+        stateMachine.sendEvent(BookEvents.RETURN);
+        stateMachine.sendEvent(BookEvents.BORROW);
+        stateMachine.stop();
+    }
+}
