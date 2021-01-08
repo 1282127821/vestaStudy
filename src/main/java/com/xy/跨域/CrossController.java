2a3dab8ddbdebbc3652d723c4c5a0abb0e1fe1cb
hugh <1282127821@qq.com>
2021-01-08 23:37:21 +0800

    2021年1月8日22:11:34

--- src/main/java/com/xy/跨域/CrossController.java
+++ src/main/java/com/xy/跨域/CrossController.java
@@ -3,11 +3,14 @@ package com.xy.跨域;/**
  * @date 2021/1/8
  */
 
+import com.alibaba.fastjson.JSON;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 /**
@@ -17,12 +20,29 @@ import javax.servlet.http.HttpSession;
  * @description:
  */
 @Controller
-@CrossOrigin(origins = "*", maxAge = 3600)
+@CrossOrigin(origins = "*" ,allowCredentials = "true",maxAge = 3600)
 public class CrossController {
     @RequestMapping("/login")
     @ResponseBody
-    public String login() {
+    public String login(HttpServletRequest request) {
+        Cookie[] cookies = request.getCookies();
+        if (cookies == null) {
+            return "获取不到cookie";
+        }
+        for (Cookie cookie : cookies) {
+            System.out.println(cookie.getName() + ">>" + cookie.getValue());
+        }
         return "返回的数据";
+    }
+
+    @RequestMapping("/index")
+    @ResponseBody
+    public String index(HttpServletRequest request) {
+        Cookie[] cookies = request.getCookies();
+        for (Cookie cookie : cookies) {
+            System.out.println(cookie.getName() + ">>" + cookie.getValue());
+        }
+        return "index正常方式";
     }
 
 }
